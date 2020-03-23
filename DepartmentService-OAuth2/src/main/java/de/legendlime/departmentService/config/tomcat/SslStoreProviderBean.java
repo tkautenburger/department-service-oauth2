ed0e1204b8c7f88316a4bcf562ec5917a86b3386
Thomas Kautenburger <tkautenburger@legendlime.de>
2020-03-23 18:00:38 +0100

    enable persistent key store for Kafka client

--- DepartmentService-OAuth2/src/main/java/de/legendlime/departmentService/config/tomcat/SslStoreProviderBean.java
+++ DepartmentService-OAuth2/src/main/java/de/legendlime/departmentService/config/tomcat/SslStoreProviderBean.java
@@ -1,6 +1,12 @@
 package de.legendlime.departmentService.config.tomcat;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.server.SslStoreProvider;
@@ -8,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import de.legendlime.departmentService.config.cert.KeyStoreBean;
 import de.legendlime.departmentService.config.cert.TrustStoreBean;
+import de.legendlime.departmentService.config.vault.VaultPkiProperties;
 
 /**
  * Spring bean containing SSL configuration with key store and trust store
@@ -24,10 +31,20 @@ import de.legendlime.departmentService.config.cert.TrustStoreBean;
 public class SslStoreProviderBean implements SslStoreProvider {
 	
 	private KeyStore keyStore, trustStore;
+	private VaultPkiProperties pkiProperties;
 	
-	public SslStoreProviderBean(KeyStoreBean keyStoreBean, TrustStoreBean trustStoreBean) {
+	public SslStoreProviderBean(KeyStoreBean keyStoreBean, TrustStoreBean trustStoreBean, VaultPkiProperties pkiProperties) 
+			throws FileNotFoundException, IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
 		this.keyStore = keyStoreBean.getKeyStore();
 		this.trustStore = trustStoreBean.getTrustStore();
+		this.pkiProperties = pkiProperties;
+		if (pkiProperties.isPersistEnabled()) {
+			// store keystore in file system
+			char[] pwdArray = System.getenv("KEY_STORE_PASSWORD").toCharArray();
+			try (FileOutputStream fos = new FileOutputStream(System.getenv("KEY_STORE_PATH"))) {
+				this.keyStore.store(fos, pwdArray);
+			}
+		}
 	}
 
 	@Override
@@ -40,8 +57,16 @@ public class SslStoreProviderBean implements SslStoreProvider {
 		return trustStore;
 	}	
 	
-	public void renew(KeyStoreBean keyStoreBean, TrustStoreBean trustStoreBean) {
+	public void renew(KeyStoreBean keyStoreBean, TrustStoreBean trustStoreBean) 
+			throws FileNotFoundException, IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
 		this.keyStore = keyStoreBean.getKeyStore();
 		this.trustStore = trustStoreBean.getTrustStore();
+		if (pkiProperties.isPersistEnabled()) {
+			// store keystore in file system
+			char[] pwdArray = System.getenv("KEY_STORE_PASSWORD").toCharArray();
+			try (FileOutputStream fos = new FileOutputStream(System.getenv("KEY_STORE_PATH"))) {
+				this.keyStore.store(fos, pwdArray);
+			}
+		}
 	}
 }
