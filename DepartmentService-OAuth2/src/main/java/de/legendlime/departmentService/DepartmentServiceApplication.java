3d4e642fca7ed8ed67e6151ebc34e66f5649230c
Thomas Kautenburger <tkautenburger@legendlime.de>
2020-07-04 19:33:02 +0200

    Rewind to non multistaged image deployment

--- DepartmentService-OAuth2/src/main/java/de/legendlime/departmentService/DepartmentServiceApplication.java
+++ DepartmentService-OAuth2/src/main/java/de/legendlime/departmentService/DepartmentServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication
 @EnableBinding(Source.class)
 public class DepartmentServiceApplication {
 	
